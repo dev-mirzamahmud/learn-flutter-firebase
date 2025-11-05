2ee130f1bd845592497c8ffeb3ab61bc4e05a753
mirzamahmud <dev.mirzamahmud@gmail.com>
2025-11-06 02:33:04 +0600

    create result class for handling data response

---
+++ lib/core/utils/result.dart
@@ -0,0 +1,13 @@
+sealed class Result<T> {
+  const Result();
+}
+
+class Success<T> extends Result<T> {
+  final T data;
+  const Success(this.data);
+}
+
+class Failure<T> extends Result<T> {
+  final String msg;
+  const Failure(this.msg);
+}
